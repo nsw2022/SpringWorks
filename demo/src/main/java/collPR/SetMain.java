ddaf563d726dfd518623a49de143507929726695
nsw2022 <tmddn3410@naver.com>
2023-08-09 18:35:19 +0900

    08_09tree_set

---
+++ demo/src/main/java/collPR/SetMain.java
@@ -0,0 +1,17 @@
+package collPR;
+
+import java.util.Set;
+
+public class SetMain {
+	public static void main(String[] args) {
+		// Set 인터페이스 : 순서 X, index X, 중복데이터 허용 X
+		
+		// Set<String> set = new Set<String>;//interface 직접 생성 불가
+		
+		// Set인터페이스를 구현한 하위 클래스들을 사용 : HashSet, TreeSet, LinckedHashSet
+		// 1) HashSet : 저장순서 파악하기 어려움. 해시 테이블이라는 것에 의해서 순서 결정
+		//              Set계열중에서 성능이 가장 좋음.
+		// 리빙 포인트 해시 테이블란? 
+		
+	}
+}
